10e17c49e74a14a9717b702f4bbdce5a5920e3ca
Waseem-Mosam <waseem.mosam1@gmail.com>
2025-07-24 09:51:40 +0200

    feat: metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,28 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-})
+    subsets: ["latin"],
+    variable: "--font-inter",
+    display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "PrimeRealty - Your Trusted Real Estate Partner",
-  description:
-    "Find your dream home with PrimeRealty. Professional real estate services with comprehensive solutions tailored to your needs.",
-    generator: 'v0.dev'
-}
+    title: "MG Properties - your space, our expertise",
+    description:
+        "Find your dream home with MG Properties. Professional real estate services with comprehensive solutions tailored to your needs.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={inter.variable}>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className={inter.variable}>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
