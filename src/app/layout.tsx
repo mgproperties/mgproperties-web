c99b590bc79e4d905ea6158016251ba7d67f06b5
Junior Moremong <jmoremong@hotmail.com>
2025-09-05 17:13:36 +0200

    feat: vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { PropertiesProvider } from "@/contexts/PropertiesContext";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
     subsets: ["latin"],
@@ -32,6 +33,7 @@ export default function RootLayout({
                 <PropertiesProvider>
                     {children}
                     <Analytics />
+                    <SpeedInsights />
                 </PropertiesProvider>
             </body>
         </html>
